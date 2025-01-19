c415c1a4a93b1f68567fab2af10805a8cdedcc2f
Dhruva Jothik Byrapatna <dhruvajb@gmail.com>
2025-01-19 15:57:54 -0500

    Helper funcs to load and save data

---
+++ analyzer/store_load_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pickle
+
+def save_df(df, path):
+    with open(path, "wb") as f:
+        pickle.dump(df, f)
+
+def save_dict(data_dict, path):
+    with open(path, "wb") as f:
+        pickle.dump(data_dict, f)
+
+def load_df(df, path):
+    with open(path, "rb") as f:
+        loaded_data = pickle.load(f)
+    return loaded_data
+
+def load_dict(data_dict, path):
+    with open(path, "rb") as f:
+        loaded_data_dict = pickle.load(f)
+    return loaded_data_dict
