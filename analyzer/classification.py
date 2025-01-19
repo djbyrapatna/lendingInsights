86ab7e46127a10367f491a9e68e4c9ca8bebc6c1
Dhruva Jothik Byrapatna <dhruvajb@gmail.com>
2025-01-19 17:18:07 -0500

    Modularization

--- analyzer/classification.py
+++ analyzer/classification.py
@@ -1,20 +1,23 @@
 import sys
 from data_parser import extract_data as ed
-from .createTrainingDataset import export_transactions_for_labeling, cluster_transaction_descriptions_pytorch
+from .createTrainingDataset import cluster_transaction_descriptions_with_amounts_split_pytorch as cluster_func
+from .store_load_data import save_dict, load_dict
 
 if __name__ == '__main__':
-    
-    files = [0,1,3]
-    dataDirectory = {}
-    for num in files:
-        pdf_file = 'pdfData/Untitled'+str(num)+'.pdf'
-        data = ed.data_extract_and_clean_pipeline(pdf_file)
+    loadPath = sys.argv[1]
+    savePath = sys.argv[2]
+    # files = [0,1,3]
+    # dataDirectory = {}
+    # for num in files:
+    #     pdf_file = 'pdfData/Untitled'+str(num)+'.pdf'
+    #     data = ed.data_extract_and_clean_pipeline(pdf_file)
 
-        dataDirectory[num] = data
-    
+    #     dataDirectory[num] = data
+    # save_dict(dataDirectory, path)
+    dataDirectory = load_dict(loadPath)
     for key in dataDirectory.keys():
         
-        clusters  = cluster_transaction_descriptions_pytorch(dataDirectory[key])
+        clusters  = cluster_func(dataDirectory[key], amount_scale=.1, model_name='mgrella/autonlp-bank-transaction-classification-5521155')
         print(clusters)
 
     
