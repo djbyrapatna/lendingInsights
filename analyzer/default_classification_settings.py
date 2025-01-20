98ef59de476a9ad2538f4d534c23c8d92d6e83bf
Dhruva Jothik Byrapatna <dhruvajb@gmail.com>
2025-01-20 18:40:57 -0500

    Updates

--- analyzer/default_classification_settings.py
+++ analyzer/default_classification_settings.py
@@ -1,4 +1,7 @@
 DEFAULT_CLASSIFICATION_MODEL = "valhalla/distilbart-mnli-12-3"
 DEFAULT_CANDIDATE_LABELS = ["Rent", "Salary", "Utilities", "Bill payment", "Transfer", "Entertainment", "Food", "Medical", "Other Bills", "Other"]
 DEFAULT_CONTEXT = 'These are bank transaction statements: '
+DEFAULT_DISCRETIONARY_CATEGORIES = {"Food", "Medical", "Entertainment"}
 DEFAULT_EMBEDDING_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
+DEFAULT_ESSENTIAL_CATEGORIES = {"Rent", "Utilities", "Bill Payment"}
+
