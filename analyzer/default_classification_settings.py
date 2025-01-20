643033602e37232225bab2f392e93da87845dee6
Dhruva Jothik Byrapatna <dhruvajb@gmail.com>
2025-01-20 17:30:34 -0500

    Holder file for default settings

---
+++ analyzer/default_classification_settings.py
@@ -0,0 +1,4 @@
+DEFAULT_CLASSIFICATION_MODEL = "valhalla/distilbart-mnli-12-3"
+DEFAULT_CANDIDATE_LABELS = ["Rent", "Salary", "Utilities", "Transfer", "Entertainment", "Food", "Medical", "Other Bills", "Other"]
+DEFAULT_CONTEXT = 'These are bank transaction statements: '
+DEFAULT_EMBEDDING_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
