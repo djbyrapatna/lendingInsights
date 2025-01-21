04c51ca6a9044883ea5380ad623518091965104b
Dhruva Jothik Byrapatna <dhruvajb@gmail.com>
2025-01-21 16:57:56 -0500

    Adding applicant name to records, bug fixing

--- frontend/my-loan-app/src/services/api.ts
+++ frontend/my-loan-app/src/services/api.ts
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 const BASE_URL = "http://127.0.0.1:8000/api";
 
-export const uploadPDF = async (file: File) => {
+export const uploadPDF = async (file: File, applicantName: string) => {
   const formData = new FormData();
   formData.append('file', file);
+  formData.append('applicantName', applicantName);
   
   const response = await axios.post(`${BASE_URL}/pdf/process`, formData, {
     headers: {
